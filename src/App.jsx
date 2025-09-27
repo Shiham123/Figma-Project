125a770296db988df42588ed4e68716bea4b38db
Shiham123 <s36651786@gmail.com>
2025-09-27 12:49:18 +0600

    just hero section added to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import HeroSection from "./Pages/home/heroSection";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="font-extrabold text-9xl">Demo class</h1>
-    </div>
+    <>
+      <HeroSection />
+    </>
   );
 };
 
---
+++ src/Pages/home/heroSection.jsx
@@ -0,0 +1,9 @@
+const heroSection = () => {
+  return (
+    <div>
+      <h1>this is home page hero heroSection</h1>
+    </div>
+  );
+};
+
+export default heroSection;
